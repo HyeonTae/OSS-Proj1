a3dd7844d1468c1a39abb53dabbba763a9e81392
Highsg19101 <Highsg19101@gmail.com>
2016-10-24 17:04:32 +0900

    Update main.c

--- main.c
+++ main.c
@@ -8,13 +8,13 @@ typedef struct node{
   struct node *next;
 }
 node *head ;
-void firstlink(int data);
+void firstinsert(char data);
 void main() {
   head = NULL;
   printf("2011244116 Test!!\n");
   prtinf("2011244119 Test!!\n");
 }
-void firstlink(char data){
+void firstinsert(char data){
 node *newNode = (node*)malloc(sizeof(node));
 newNode = head;  
 newNode -> name = data;

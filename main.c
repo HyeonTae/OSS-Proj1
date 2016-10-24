4bdd66d983ac8ada2de8569bde656b23a69a77d5
Highsg19101 <Highsg19101@gmail.com>
2016-10-24 17:06:05 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+//2013244051 kosanggyun
 typedef struct node{
   char name[10];
   int age;
@@ -13,6 +13,7 @@ void main() {
   head = NULL;
   printf("2011244116 Test!!\n");
   prtinf("2011244119 Test!!\n");
+  printf("2013244051 Test!!\n");
 }
 void firstinsert(char data){
 node *newNode = (node*)malloc(sizeof(node));
